06177c91f30101018716752c1ec599480b15b968
Aneudy Abreu <aneudy.abreureynoso@aexp.com>
2019-05-27 12:51:23 -0400

    added service for distance zipcode

--- services/distance.js
+++ services/distance.js
@@ -1,7 +1,35 @@
+const request = require('request');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const getApiUrl = req => [
+  process.env.ZIP_CODE_API_URL,
+  process.env.ZIP_CODE_API_KEY,
+  'distance.json',
+  req.params.zipcode1,
+  req.params.zipcode2,
+  'mile'
+].join('/');
+
 const find = (req, res, cb) => {
-  cb(null, {
-    far: true
-  })
+  request(getApiUrl(req), (err, response, body) => {
+    let distanceData = {
+      distance: -1
+    };
+    if (!err) {
+      try {
+        distanceData = JSON.parse(body)
+      } catch (error) {
+        console.log({
+          error
+        })
+      }
+    }
+    console.log({
+      err, distanceData, body, code: response.code
+    })
+    cb(err, distanceData)
+  });
 };
 
 module.exports = {
